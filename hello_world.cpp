fa6406cc8cf56f5161a9d9451f1c07ad76dd3d1a
Alyonkachocolate <Nyanyoka@yandex.ru>
2020-04-24 22:14:55 +0400

    Change the code style with clang-format utility

---
+++ hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+int main() {
+  std::string name;  // user name
+  std::cin >> s;     // input user name
+  std::cout << "Hello world " << name << std::endl;
+  return 0;
+}
